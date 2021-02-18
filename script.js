e3f476f338b03b83576cbb2dc010a61895bafb3d
Kairat <karik-x@mail.ru>
2021-02-18 15:53:38 +0300

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+$(".main__menu li ul").hide();
+
+$(".main__menu li:has('.submenu')").hover(
+    function() {
+        // $(this).addClass("active");
+        $(this).find('ul').slideDown();
+    },
+    function() {
+        // $(this).removeClass("active");        
+        $(this).find('ul').slideUp('fast');
+    }
+//   function(){
+//   $(".main__menu li ul").stop().fadeToggle(300);}
+);
+
+});
+
+
+
+
+
+
+
